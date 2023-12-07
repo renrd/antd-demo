ba6cae28bd421c4475271ff372bbf306303ba40c
renard121 <1179327883@qq.com>
2023-12-07 16:39:57 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+/*
+ * @Author: renard121 1179327883@qq.com
+ * @Date: 2023-12-07 15:58:48
+ * @LastEditors: renard121 1179327883@qq.com
+ * @LastEditTime: 2023-12-07 16:39:36
+ * @FilePath: \antd-demo\src\main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
+import router from '.router'
+import store from './store'
 
-createApp(App).mount('#app')
+createApp(App).user(Antd).use(store).use(router).mount('#app')
